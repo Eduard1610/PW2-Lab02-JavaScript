0ffa73443ac67d26192dc69eeb7cd191ed453ee7
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-05-05 13:08:16 -0500

    Ejercicio 1 - Creé variable nombre que indicará el día y un switch que le de el valor a nombre según el día que sea

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -4,6 +4,31 @@ function indicadorDia() {
     const d = new Date();
     let day = d.getDay();
     console.log(day);
+    let nombre = "";
+    switch (day){
+        case 0:
+            nombre = "domingo";
+            break;
+        case 1:
+            nombre = "lunes";
+            break;
+        case 2:
+            nombre = "martes";
+            break;
+        case 3:
+            nombre = "miércoles";
+            break;
+        case 4:
+            nombre = "jueves";
+            break;
+        case 5:
+            nombre = "viernes";
+            break;
+        case 6:
+            nombre = "sábado";
+            break;
+    }
+    console.log(nombre);
 }
 
 indicadorDia();
