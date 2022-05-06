0794f16548e644997bb4667dd6531b9aaf963186
JChua21 <jchua@unsa.edu.pe>
2022-05-06 12:49:14 -0500

    Capture el input del usuario y lo muestro en un nuevo div

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,1 +1,18 @@
-console.log("Ingrese");
+
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click" , function(){
+
+    //Obtengo el input donde el usuario escribio la palabra
+    const inputUser = document.getElementById("palabra");
+
+    //console.log(inputUser.value);
+
+    //Creo un nuevo div que se mostrará
+    const resultado = document.createElement("DIV");
+    resultado.innerHTML = `
+        <p> El texto invertido es  : <b> ${inputUser.value} </b><p>
+        `
+    const body = document.querySelector("body");
+    body.appendChild(resultado);
+});
